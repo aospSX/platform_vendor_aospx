92a48617d7e230bcccad45bf2d68c41d543b9381
LuisCruz <ljc2491@gmail.com>
2012-10-24 03:43:32 -0500

    Include and build AospXStockBlu theme

--- products/themes_common.mk
+++ products/themes_common.mk
@@ -2,7 +2,8 @@
 PRODUCT_PACKAGES += \
        ThemeManager \
        ThemeChooser \
-       com.tmobile.themes
+       com.tmobile.themes \
+       AospXStockBlu
 
 PRODUCT_COPY_FILES += \
        vendor/aospx/products/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
